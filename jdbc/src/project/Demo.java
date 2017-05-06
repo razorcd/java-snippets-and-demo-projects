199acf052c2af7fcbaa1e7256fd4211ae1692d9e
Cristian Dugacicu <razorcd@yahoo.com>
2017-05-06 19:00:02 +0100

    adds db conncetion and table creation

--- jdbc/src/project/Demo.java
+++ jdbc/src/project/Demo.java
@@ -1,11 +1,27 @@
 package project;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Demo {
 
-    public static void main(String[] args) {
+    static String baseUrl = "jdbc:mysql://" + "localhost" + ":" + "3306" + "/mysql?useSSL=false";
+
+    public static void main(String[] args) throws SQLException {
         System.out.println("START");
+
+        // 1. Create database and table for our demo
+        Connection rawConnection = DriverManager.getConnection(baseUrl, "root", "---");
+        Statement statement = rawConnection.createStatement();
+        rawConnection.setAutoCommit(false); //we are committing everything explicitly
+
+        statement.executeUpdate("create database if not exists persons");
+        statement.close();
+
+        rawConnection.commit();
+        rawConnection.close();
     }
 
 }
