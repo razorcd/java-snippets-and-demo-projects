6d8baab94acd7397a03bc975d2ea5e3adcc1612b
Cristian Dugacicu <razorcd@yahoo.com>
2017-05-27 20:17:04 +0100

    adds tests for localTime endpoint

---
+++ springBootSnippets/src/test/java/com/example/demo/controller/MyControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.myConfigurations.localTimeFactory.LocalTimeFacotry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.BDDMockito.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyControllerTest {
+
+    @Autowired
+    MyController myController;
+
+    @MockBean
+    LocalTimeFacotry localTimeFacotry;
+
+    @Test
+    public void testLocalTime() throws InterruptedException{
+        given(localTimeFacotry.now()).willReturn(LocalDateTime.of(2018,1,18,1,5,54));
+        String mockedTime = "2018-01-18T01:05:54<br/>2018-01-18T01:05:54";
+        
+        assertEquals("Should return time from LocalTimeFactory", mockedTime, myController.localTime());
+    }
+}
