35189bc569286ab3e548f452717a8054eb0985d7
Cristian Dugacicu <razorcd@yahoo.com>
2017-09-02 18:15:36 +0100

    more networking

--- networking/src/com/demo/Main.java
+++ networking/src/com/demo/Main.java
@@ -2,8 +2,11 @@ package com.demo;
 
 import com.demo.socket_connection.ClientDemo;
 import com.demo.socket_connection.ServerSocketDemo;
+import sun.net.www.http.HttpClient;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class Main {
 
@@ -11,10 +14,15 @@ public class Main {
 
 //        HttpConn.run();   // get HTTP connection
 
-        TCPIPSockets.run();
+//        TCPIPSockets.run();
 
 
         // for Server/Client Socket please build and run both  ServerSocketDemo  and   ClientDemo   at the same time. Then send message form one to another.
+
+        HttpClient httpClient = HttpClient.New(new URL("http://www.google.com"));
+        httpClient.
+        InputStream inputStream = httpClient.getInputStream();
+        System.out.println(inputStream.read());
 
     }
 }
--- networking/src/com/demo/socket_connection/ClientDemo.java
+++ networking/src/com/demo/socket_connection/ClientDemo.java
@@ -1,5 +1,7 @@
 package com.demo.socket_connection;
 
+import sun.net.www.http.HttpClient;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
